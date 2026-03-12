14554ed458d194fc94690699dd948c876b374258
Matheus Muniz <i.am.matheus.muniz@gmail.com>
2026-03-12 09:51:25 -0300

    Some more cs50p exercises

---
+++ hogwarts.py
@@ -0,0 +1,18 @@
+# students = ["Hermione", "Harry", "Ron"]
+
+#List within range in Loop
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+###
+#Perfect Example for exhibiting the list
+# for student in students:
+#     print(student)
+###
+#Dictionary example
+students = {
+    "Hermione": "Gryffindor", 
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin"
+    }
+
